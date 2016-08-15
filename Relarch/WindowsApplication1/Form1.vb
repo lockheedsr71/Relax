8c0559f87bc4ae916513038df497a372d01174b9
lockheedsr71 <mbksoft@gmail.com>
2016-08-15 16:50:25 +0430

    first version

---
+++ Relarch/WindowsApplication1/Form1.vb
@@ -0,0 +1,72 @@
+﻿Public Class Form1
+
+
+    Dim sourcepath As String
+    Dim despath As String
+
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        If (FolderBrowserDialog1.ShowDialog() = DialogResult.OK) Then
+            TextBox1.Text = FolderBrowserDialog1.SelectedPath
+
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If (FolderBrowserDialog1.ShowDialog() = DialogResult.OK) Then
+            TextBox2.Text = FolderBrowserDialog1.SelectedPath
+
+        End If
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Application.Exit()
+
+
+
+
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        sourcepath = TextBox1.Text
+        AboutBox1.Show()
+
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+        sourcepath = TextBox1.Text
+        despath = TextBox2.Text
+        My.Computer.FileSystem.CopyDirectory(sourcepath, despath, showUI:=FileIO.UIOption.AllDialogs)
+        MessageBox.Show("aaaaa")
+
+
+    End Sub
+End Class
+
+
+
